3291c0ff0cc90006b363ed5fc2fbcf5383cc5ce2
Ivan Laptev <ivlaptev13@ya.ru>
2019-03-03 10:37:15 +0300

    Added 7.2

---
+++ Pr7.2/Pr7.2.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Figure {	// Базовый абстрактный класс "Фигура"
+protected:
+	int vertices;	// Количество вершин
+public:
+	Figure(int vertices) { this->vertices = vertices; }
+
+	virtual double getSquare() = 0;	// Виртуальный метод, считающий площадь
+};
+
+class Quadrilateral : public Figure{	// Абстрактный класс "Четырёхугольник"
+protected:
+	vector<double> x;	// х составляющая координат точек
+	vector<double> y;	// у составляющая координат точек
+public:
+	Quadrilateral(vector<double> &x, vector<double> &y) : Figure(4) {
+		this->x = x;
+		this->y = y;
+	}
+};
+
+class Rhombus : public Quadrilateral {	// Класс "Ромб"
+public:
+	Rhombus(vector<double> &x, vector<double> &y) :
+		Quadrilateral(x, y) {};
+
+	double getSquare() {	// Определённый метод для подсчёта площади фигуры
+		return sqrt((x[0] - x[2]) * (x[0] - x[2]) + (y[0] - y[2]) * (y[0] - y[2])) *
+				sqrt((x[1] - x[3]) * (x[1] - x[3]) + (y[1] - y[3]) * (y[1] - y[3])) / 2;
+	}
+
+	~Rhombus() {
+	}
+};
+
+class Rectangle : public Quadrilateral {	// Класс "Прямоугольник"
+public:
+	Rectangle(vector<double> &x, vector<double> &y) :
+		Quadrilateral(x, y) {};
+
+	double getSquare() {	// Определённый метод для подсчёта площади фигуры
+		return sqrt((x[0] - x[1]) * (x[0] - x[1]) + (y[0] - y[1]) * (y[0] - y[1])) *
+					sqrt((x[2] - x[3]) * (x[2] - x[3]) + (y[2] - y[3]) * (y[2] - y[3]));
+	}
+
+	~Rectangle() {}
+};
+
+int main() {
+	vector<double> x1(4);
+	vector<double> y1(4);
+
+	vector<double> x2(4);
+	vector<double> y2(4);
+
+	for (int i = 0; i < 4; i++) {
+		cout << "Enter rhombus vertex " << i + 1 << ": ";
+		cin >> x1[i] >> y1[i];
+	}
+	for (int i = 0; i < 4; i++) {
+		cout << "Enter rectangle vertex " << i + 1 << ": ";
+		cin >> x2[i] >> y2[i];
+	}
+
+	Rhombus r(x1, y1);
+	Rectangle rect(x2, y2);
+	cout << "Rhombus square: " << r.getSquare() << endl;
+	cout << "Rectangle square: " << rect.getSquare() << endl;
+
+	system("pause");
+	return 0;
+}
