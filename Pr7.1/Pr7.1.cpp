ee40d14ad6906f734014f953b94eee10d757db0e
Ivan Laptev <ivlaptev13@ya.ru>
2019-03-17 09:43:06 +0300

    Update pr7.1

--- Pr7.1/Pr7.1.cpp
+++ Pr7.1/Pr7.1.cpp
@@ -8,11 +8,7 @@ protected:
 	string name;
 	int age;
 public:
-	Animal(string &name, int legs, int age) {
-		this->name = name;
-		this->age = age;
-		this->legs = legs;
-	}
+	Animal(string&, int, int);
 
 	void setName(string &name) { this->name = name; }
 
@@ -33,27 +29,21 @@ class Cat : public Animal {	// Производный класс "Кошка"
 public:
 	Cat(string &name, int legs, int age) : Animal(name, legs, age) {}
 
-	void say() {
-		cout << "Meow" << endl;
-	}
+	void say() { cout << "Meow" << endl; }
 };
 
 class Dog : public Animal {	// Производный класс "Собака"
 public:
 	Dog(string &name, int legs, int age) : Animal(name, legs, age) {}
 
-	void say() {
-		cout << "Woof" << endl;
-	}
+	void say() { cout << "Woof" << endl; }
 };
 
 class Parrot : public Animal {	// Производный класс "Попугай"
 public:
 	Parrot(string &name, int legs, int age) : Animal(name, legs, age) {}
 
-	void say() {
-		cout << "I am man!" << endl;
-	}
+	void say() { cout << "I am man!" << endl; }
 };
 
 int main() {
@@ -72,4 +62,10 @@ int main() {
 
 	system("pause");
 	return 0;
+}
+
+Animal::Animal(string &name, int legs, int age) {
+	this->name = name;
+	this->age = age;
+	this->legs = legs;
 }
